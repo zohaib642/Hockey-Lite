3e710d97c5412d3892a5e3b0f51e1f7e4028248f
Zohaib Sheikh <92965609+zohaib642@users.noreply.github.com>
2025-01-10 11:01:59 -0600

    stuff

---
+++ scenes/move.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var speed = 2000
+# Called when the node enters the scene tree for the first time.
+
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D):
+		if position.x < get_tree().root.get_viewport().get_size().x/2.1:
+			position.x += speed * delta
+	if Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S):
+		position.y += speed * delta
+		
